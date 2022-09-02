7368591b6c2ab031ae8a7906bb63e3c11f50cb7f
Md.Masud Parvez <pavelcep.sust11@gmail.com>
2022-09-03 04:57:29 +0600

    reverse words added

--- src/stringproblem/ReverseSentence.java
+++ src/stringproblem/ReverseSentence.java
@@ -3,24 +3,45 @@ package stringproblem;
 public class ReverseSentence {
 
     public static void main(String[] args) {
-        String sentence = " Hello World ";
+        String sentence = " Hello World pavel ";
         System.out.println(reverseWords(sentence));
     }
 
+//    public static String reverseWords(String s) {
+//        // Trim leading and trailing spaces
+//        s = s.trim();
+//        // Replace multiple consecutive spaces with single space
+//        s = s.replaceAll(" +", " ");  // replace all multiple space with single space
+//        String[] words = s.split(" "); // split words according to space
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i=words.length-1 ; i>=0;i--){
+//            if (i==words.length-1){  // last words insert into first
+//                sb.append(words[i]);
+//            } else {
+//                sb.append(" "); // add space after first word
+//                sb.append(words[i]); // add words
+//            }
+//        }
+//        return sb.toString();  // convert StringBuilder to string
+//    }
+
 
     public static String reverseWords(String s) {
-        // Trim leading and trailing spaces
         s = s.trim();
-        // Replace multiple consecutive spaces with single space
-        s = s.replaceAll(" ", " ");
+        s = s.replaceAll(" +", " ");
         String[] words = s.split(" ");
-        StringBuilder sb = new StringBuilder(s.length());
-        for (int i = words.length-1; i > 0; i--) {
-            sb.append(words[i]);
-            sb.append("1 ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length-1; i>=0; i--){
+            if (i ==words.length-1){
+                sb.append(words[i]);
+            }else{
+                sb.append(" ");
+                sb.append(words[i]);
+            }
         }
-        sb.append(words[0]);
         return sb.toString();
     }
-
 }
+
+
