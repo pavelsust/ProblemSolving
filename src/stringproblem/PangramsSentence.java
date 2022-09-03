2303f5cdbbe76e3c5acc6fab737435779faa76d0
Md.Masud Parvez <pavelcep.sust11@gmail.com>
2022-09-04 01:24:29 +0600

    pangram sentence

---
+++ src/stringproblem/PangramsSentence.java
@@ -0,0 +1,32 @@
+package stringproblem;
+
+public class PangramsSentence {
+
+    public static void main(String[] args) {
+        String str = "The quick brown fox jumps over the lazy dog";
+        System.out.println(checkPanfram(str));
+    }
+
+    public static boolean checkPanfram(String s){
+        boolean[] marks = new boolean[26];
+        int index =0;
+
+        for (int i =0; i< s.length(); i++){
+            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z'){
+                index = s.charAt(i)-'A';
+            }else if ('a' <=s.charAt(i) && s.charAt(i)<='z'){
+                index = s.charAt(i)-'a';
+            } else {
+                continue;
+            }
+            marks[index] = true;
+        }
+
+        // Return false if any character is unmarked
+        for (int i = 0; i <= 25; i++)
+            if (!marks[i])
+                return (false);
+
+        return (true);
+    }
+}
